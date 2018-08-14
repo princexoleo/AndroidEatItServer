34baa3f54c802107fd4ddc657b97216c80b923bb
princexoleo <xtleon.xl@gmail.com>
2018-08-15 03:39:37 +0600

    *Admin can now change order status *order delete option added

--- app/src/main/java/com/app/leo/androideatitserver/OrderStatus.java
+++ app/src/main/java/com/app/leo/androideatitserver/OrderStatus.java
@@ -1,10 +1,16 @@
 package com.app.leo.androideatitserver;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.LayoutInflater;
+
 
 import com.app.leo.androideatitserver.Common.Common;
 import com.app.leo.androideatitserver.Interface.ItemClickListener;
@@ -13,6 +19,7 @@ import com.app.leo.androideatitserver.ViewHolder.OrderViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class OrderStatus extends AppCompatActivity {
 
@@ -22,7 +29,8 @@ public class OrderStatus extends AppCompatActivity {
     FirebaseRecyclerAdapter<Request,OrderViewHolder>adapter;
 
     FirebaseDatabase db;
-    DatabaseReference request;
+    DatabaseReference requestRef;
+    MaterialSpinner materialSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,7 @@ public class OrderStatus extends AppCompatActivity {
 
         //init firebase
         db=FirebaseDatabase.getInstance();
-        request=db.getReference("Requests");
+        requestRef =db.getReference("Requests");
 
         //init view
         recyclerView=findViewById(R.id.listOrders);
@@ -47,7 +55,7 @@ public class OrderStatus extends AppCompatActivity {
                 Request.class,
                 R.layout.order_layout,
                 OrderViewHolder.class,
-                request
+                requestRef
         ) {
             @Override
             protected void populateViewHolder(OrderViewHolder viewHolder, Request model, int position) {
@@ -67,5 +75,59 @@ public class OrderStatus extends AppCompatActivity {
         };
         adapter.notifyDataSetChanged();
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if(item.getTitle().equals(Common.UPDATE))
+        {
+            showUpdateDialog(adapter.getRef(item.getOrder()).getKey(), 
+                    adapter.getItem(item.getOrder()));
+        }
+        else if(item.getTitle().equals(Common.DELETE))
+        {
+            deleteOrder(adapter.getRef(item.getOrder()).getKey());
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteOrder(String key) {
+
+        requestRef.child(key).removeValue();
+    }
+
+    private void showUpdateDialog(String key, final Request item) {
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(OrderStatus.this);
+        alertDialog.setTitle("Update Orders");
+        alertDialog.setMessage("Please change order status: ");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View view = inflater.inflate(R.layout.update_lorder_layout,null);
+
+        materialSpinner= view.findViewById(R.id.statusSpinnerID);
+        materialSpinner.setItems("Placed","On my way","Shipped");
+
+        alertDialog.setView(view);
+
+        final String localKey= key;
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                item.setStatus(String.valueOf(materialSpinner.getSelectedIndex()));
+
+                requestRef.child(localKey).setValue(item);
+            }
+        });
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+
     }
 }
